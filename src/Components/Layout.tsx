fa3d7f4382e139d1ff9cd1b8cef40f16d63f04b2
chinaza ogbonna <wondernaza@yahoo.com>
2025-02-26 11:17:34 +0000

    added footer

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,10 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { IoMdClose } from 'react-icons/io';
 import { RiArrowDropDownLine } from 'react-icons/ri';
 import { FiSearch } from 'react-icons/fi';
 import { LuShoppingCart } from 'react-icons/lu';
 import { VscAccount } from 'react-icons/vsc';
 import { HiOutlineMailOpen } from 'react-icons/hi';
+import { AiFillTwitterCircle } from 'react-icons/ai';
+import { PiFacebookLogoFill } from 'react-icons/pi';
+import { IoLogoInstagram } from 'react-icons/io';
+import { FaSnapchat } from 'react-icons/fa';
+import { FaCcVisa } from 'react-icons/fa';
+import { RiMastercardFill } from 'react-icons/ri';
+import { FaCcPaypal } from 'react-icons/fa';
+import { FaCcApplePay } from 'react-icons/fa';
+import { FaGooglePay } from 'react-icons/fa6';
 
 const Layout = () => {
 	return (
@@ -54,8 +63,8 @@ const Layout = () => {
 			<div>
 				<Outlet />
 			</div>
-			<div className="bg-[#F0F0F0]">
-				<div className="bg-[#000000] w-[1240px] rounded-2xl flex justify-between items-center mx-auto py-10 px-20">
+			<div className="bg-[#F0F0F0] mt-32 relative pt-20 ">
+				<div className="bg-[#000000] w-[1240px] rounded-2xl flex justify-between items-center  py-10 px-20 absolute -top-24 right-64">
 					<h2 className="lato font-bold text-[40px] leading-[45px] text-[#FFFFFF] w-[551px] ">
 						STAY UPTO DATE ABOUT OUR LATEST OFFERS
 					</h2>
@@ -74,7 +83,78 @@ const Layout = () => {
 					</div>
 				</div>
 
-				<div className="bg-[#F0F0F0]"></div>
+				<div className="flex justify-between items-center py-16 w-[1240px] mx-auto">
+					<div className="space-y-9">
+						<h1 className="font-bold text-[33.45px] leading-[40.15px] text-[#000000]">SHOP.CO</h1>
+						<p className="font-normal text-[14px] leading-[22px] text-[#00000099] w-[248px]">
+							We have clothes that suits your style and which you’re proud to wear. From women to
+							men.
+						</p>
+						<div className="flex space-x-6 items-center">
+							<AiFillTwitterCircle className="text-2xl" />
+							<PiFacebookLogoFill className="text-2xl" />
+							<IoLogoInstagram className="text-2xl" />
+							<FaSnapchat className="text-2xl" />
+						</div>
+					</div>
+					<div className="space-y-7">
+						<h1 className="font-medium text-[16px] leading-[18px] text-[#000000]">COMPANY</h1>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">About</p>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">Features</p>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">Works</p>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">Career</p>
+					</div>
+					<div className="space-y-7">
+						<h1 className="font-medium text-[16px] leading-[18px] text-[#000000]">HELP</h1>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">Customer</p>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">
+							Delivery Details
+						</p>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">
+							Terms & Conditions
+						</p>
+						<p>
+							<Link to={''} className="font-normal text-[16px] leading-[19px] text-[#00000099]">
+								Privacy Policy
+							</Link>
+						</p>
+					</div>
+					<div className="space-y-7">
+						<h1 className="font-medium text-[16px] leading-[18px] text-[#000000]">FAQ</h1>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">Account</p>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">
+							Manage Deliveries
+						</p>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">Orders</p>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">Payments</p>
+					</div>
+					<div className="space-y-7">
+						<h1 className="font-medium text-[16px] leading-[18px] text-[#000000]">RESOURCES</h1>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">Free eBooks</p>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">
+							development Tutorial
+						</p>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">How to -Blog</p>
+						<p className="font-normal text-[16px] leading-[19px] text-[#00000099]">
+							Youtube playlist
+						</p>
+					</div>
+				</div>
+				<hr className="bg-[#0000001A] h-1 w-[1240px] mx-auto" />
+				<div className=" flex justify-between pb-16 pt-5 mx-auto w-[1240px]">
+					<span className="font-normal text-[14px] leading-[18.9px] text-[#00000099]">
+						© 2000-2021, All rights reserved
+					</span>
+					<div>
+						<div className="flex space-x-2">
+							<FaCcVisa className="text-2xl  text-[#1434CB]" />
+							<RiMastercardFill className="text-2xl" />
+							<FaCcPaypal className="text-2xl" />
+							<FaCcApplePay className="text-2xl" />
+							<FaGooglePay className="text-2xl " />
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
